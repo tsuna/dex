b4434265b88c02cc5a71460ef2a028c5fb479578
Benoit Sigoure <tsunanet@gmail.com>
2019-04-19 14:15:32 -0700

    metrics for CORS handlers

--- server/server.go
+++ server/server.go
@@ -256,7 +256,7 @@ func newServer(ctx context.Context, c Config, rotationStrategy rotationStrategy)
 			corsOption := handlers.AllowedOrigins(c.AllowedOrigins)
 			handler = handlers.CORS(corsOption)(handler)
 		}
-		r.Handle(path.Join(issuerURL.Path, p), handler)
+		r.Handle(path.Join(issuerURL.Path, p), instrumentHandlerCounter(p, handler))
 	}
 	r.NotFoundHandler = http.HandlerFunc(http.NotFound)
 
